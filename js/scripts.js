bda9f5b473921f643b571f9d51d678e22a981614
Charlie Kelson and Lois Choi <charlie@>
2017-07-13 09:35:38 -0700

    add HTML, add css, js, img files, add jquery to recieve input

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+
+
+$(document).ready(function() {
+  $("#number-input").submit(function(event) {
+    var number = $("input#numbers").val();
+    $("#output").text(romanNumeral(number));
+    event.preventDefault();
+  });
+});
