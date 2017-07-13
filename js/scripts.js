1bb33f9985485df225bb459a79069c14773fa9e8
Charlie Kelson and Lois Choi <lois@>
2017-07-13 12:05:20 -0700

    lunch commit.

--- js/scripts.js
+++ js/scripts.js
@@ -1,63 +1,10 @@
-var values = ['1', '5', '10', '50', '100', '500', '1000'];
-var roman = ['I', 'V', 'X', 'L', 'C', 'D', 'M'];
-var romTwo = 'II';
-var romThree = 'III';
-var romFour = 'IV';
-var romSix = 'VI';
-var romSeven = 'V' + romTwo;
-var romEight = 'V' + romThree;
-var romNine = roman[0] + roman[2];
-var romEleven = roman[2] + roman[0];
-var romTwelve = roman[2] + romTwo;
-var romThirteen = roman[2] + romThree;
-var romFourteen = roman[2] + romFour;
-var romFifteen = roman[2] + roman[1];
-var romSixteen = roman[2] + romSix;
-var romSeventeen = roman[2] + romSeven;
-var romEighteen = roman[2] + romEight;
-var romNineteen = roman[2] + romNine;
-var romTwenty = roman[2] + roman[2];
+var values = ['1000', '500', '100', '50', '10', '5', '1'];
+var roman = ['M', 'D', 'C', 'L', 'X', 'V', 'I'];
+
 
 var romanNumeral = function(number) {
-  // this for loop will equal even values and their roman counterpart
   for (i = 0; i < values.length; i++) {
-      if (number === values[i]) {
-        return roman[i];
-      } else if (number === '2') {
-        return romTwo;
-      } else if (number === '3') {
-        return romThree;
-      } else if (number === '4') {
-        return romFour;
-      } else if  (number === '6') {
-        return romSix;
-      } else if (number === '7') {
-        return romSeven;
-      } else if (number === '8') {
-        return romEight;
-      } else if (number === '9') {
-        return romNine;
-      } else if (number === '11') {
-        return romEleven;
-      } else if (number === '12') {
-        return romTwelve;
-      } else if (number === '13') {
-        return romThirteen;
-      } else if (number === '14') {
-        return romFourteen;
-      } else if (number === '15') {
-        return romFifteen;
-      } else if (number === '16') {
-        return romSixteen;
-      } else if (number === '17') {
-        return romSeventeen;
-      } else if (number === '18') {
-        return romEighteen;
-      } else if (number === '19') {
-        return romNineteen;
-      } else if (number === '20') {
-        return romTwenty;
-      }
+
   }
 }
 
@@ -70,3 +17,13 @@ $(document).ready(function() {
     event.preventDefault();
   });
 });
+
+
+// number = 6
+
+// if 6 is more than next lowest number in the values array then take that roman numeral and subract it from that roman numeral and take the remainder and turn that into a roman numeral
+
+
+if (number > values[i]) {
+  var remainder =  number - values[i];
+}
