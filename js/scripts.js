e7dc67f5b0098645d6b3f9adb96737ad4fa448b9
Charlie Kelson and Lois Choi <lois@>
2017-07-13 10:15:50 -0700

    add and test spec 1.

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,18 @@
 var values = ['1', '5', '10', '50', '100', '500', '1000'];
 var roman = ['I', 'V', 'X', 'L', 'C', 'D', 'M'];
 
+
 var romanNumeral = function(number) {
-  
+  for (i = 0; i < values.length; i++) {
+    for (j = 0; j < roman.length; i++) {
+      if (number === values[i]) {
+        return roman[i];
+      }
+      // } else if (number[1] === values[i]) {
+      //   return roman[1];
+      // }
+    }
+  }
 }
 
 
