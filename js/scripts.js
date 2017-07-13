320829253683aa68dc3a6bbc57d7011683707164
Charlie Kelson and Lois Choi <charlie@>
2017-07-13 15:16:39 -0700

    add and test spec 5 & 6 and then tested previous specs.

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,22 @@
-var values = ['1000', '500', '100', '50', '10', '5', '1'];
+var romanArr = [];
+var values = [1000, 500, 100, 50, 10, 5, 1];
 var roman = ['M', 'D', 'C', 'L', 'X', 'V', 'I'];
 
 
 var romanNumeral = function(number) {
+  var numberInt = parseInt(number);
   for (i = 0; i < values.length; i++) {
-
+      while (number >= values[i]) {
+        var firstValue = values[i];
+        romanArr.push(roman[i]);
+        console.log(romanArr);
+        console.log("number is " + number);
+        var number = (number - firstValue);
+      }
+    }
+    return romanArr.join('');
   }
-}
+
 
 
 
@@ -24,6 +34,6 @@ $(document).ready(function() {
 // if 6 is more than next lowest number in the values array then take that roman numeral and subract it from that roman numeral and take the remainder and turn that into a roman numeral
 
 
-if (number > values[i]) {
-  var remainder =  number - values[i];
-}
+// if (number > values[i]) {
+//   var remainder =  number - values[i];
+// }
